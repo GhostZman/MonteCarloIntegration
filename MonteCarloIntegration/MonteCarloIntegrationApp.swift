2db613dffb6b7cd453d4f14617d8219890c8de1e
Zachary Ives <zives@hawk.iit.edu>
2024-02-09 18:34:49 -0600

    Initial Commit

---
+++ MonteCarloIntegration/MonteCarloIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonteCarloIntegrationApp.swift
+//  MonteCarloIntegration
+//
+//  Created by Phys440Zachary on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MonteCarloIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
